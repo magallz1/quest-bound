bfbdb38af6bd751402a8e2a2d599786b048fa998
curtmorgan3 <curtmorgan3@gmail.com>
2024-12-27 11:12:42 -0700

    feat: Remove instagram cta

--- client/src/libs/compass-api/components/sign-up-form/index.ts
+++ client/src/libs/compass-api/components/sign-up-form/index.ts
@@ -1,4 +1,3 @@
-export * from './follow-instagram';
 export * from './join-discord';
 export * from './sign-up-form';
 export * from './social-links';
--- client/src/libs/compass-api/components/sign-up-form/social-links.tsx
+++ client/src/libs/compass-api/components/sign-up-form/social-links.tsx
@@ -1,6 +1,5 @@
 import { Stack } from '@/libs/compass-core-ui';
 import React from 'react';
-import { FollowInstagram } from './follow-instagram';
 import { JoinDiscord } from './join-discord';
 
 interface SocialLinksProps {
@@ -10,6 +9,5 @@ interface SocialLinksProps {
 export const SocialLinks = ({ style }: SocialLinksProps) => (
   <Stack direction='row' sx={{ justifyContent: 'space-between', width: '80px', ...style }}>
     <JoinDiscord iconButton />
-    <FollowInstagram iconButton />
   </Stack>
 );
